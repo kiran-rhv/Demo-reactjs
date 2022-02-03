8d02dc4a887356f1aececcddfa1936cd455816c7
kiran-rhv <r.kiran@dollarbirdinc.comm>
2022-02-03 17:27:13 +0530

    Creating custom components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from "./logo.svg";
+import Expenses from "./components/Expenses";
 import "./App.css";
 
 function App() {
-  return <div className="App">Monthly Expense</div>;
+  const expenses = [
+    {
+      id: "e1",
+      title: "Tooth Brush",
+      amount: 30.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 45799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 5560.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 2450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <div>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
